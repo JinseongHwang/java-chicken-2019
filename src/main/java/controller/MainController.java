eeec33cc27535620dcabdaa65f313856bd206183
JinseongHwang <eddy5360@naver.com>
2020-12-18 00:47:55 +0900

    feat: 메인화면에서 선택하는 기능 구현

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -7,15 +7,22 @@ public class MainController {
 
     public static void run() {
         MainController.print();
+        int number = InputView.inputFunctionNumber();
+
+
         Table currentTable = InputView.inputTableNumber();
         System.out.println(currentTable.toString());
     }
 
     private static void print() {
-        System.out.println("## 메인화면");
-        System.out.println("1 - 주문등록");
-        System.out.println("2 - 결제하기");
-        System.out.println("3 - 프로그램 종료");
+        System.out.println(MainFunction.TITLE.toString());
+        System.out.println(MainFunction.ORDER.toString());
+        System.out.println(MainFunction.PAYMENT.toString());
+        System.out.println(MainFunction.EXIT.toString());
+    }
+
+    private static void nextProcedure(int command) {
+
     }
 
 
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,6 +2,7 @@ package view;
 
 import domain.Table;
 import domain.TableRepository;
+import utils.Validator;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -13,6 +14,22 @@ public class InputView {
         scanner.nextLine();
     }
 
+    public static int inputFunctionNumber() {
+        try {
+            System.out.println("## 원하는 기능을 선택하세요.");
+            int functionNumber = scanner.nextInt();
+            bufferFlush();
+            return Validator.onlyMainFunction(functionNumber);
+        } catch (InputMismatchException IME) {
+            System.out.println("[ERROR] 정수를 입력해주세요.");
+            bufferFlush();
+            return inputFunctionNumber();
+        } catch (IllegalArgumentException IAE) {
+            System.out.println("[ERROR] 선택할 수 없는 기능입니다.");
+            return  inputFunctionNumber();
+        }
+    }
+
     public static Table inputTableNumber() {
         try {
             System.out.println("## 테이블을 선택하세요.");
@@ -20,7 +37,7 @@ public class InputView {
             bufferFlush();
             return TableRepository.getTablebyNumber(tableNumber);
         } catch (InputMismatchException IME) {
-            System.out.println("[ERROR] 정수를 입력해주세요..");
+            System.out.println("[ERROR] 정수를 입력해주세요.");
             bufferFlush();
             return inputTableNumber();
         } catch (IllegalArgumentException IAE) {
