5af727acc7f4a00b618632ae92737525f1539e60
JinseongHwang <eddy5360@naver.com>
2020-12-18 02:39:09 +0900

    feat: 메인 화면만 실행하도록 변경

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,22 +1,8 @@
 import controller.MainController;
-import domain.Menu;
-import domain.MenuRepository;
-import domain.Table;
-import domain.TableRepository;
-import view.InputView;
-import view.OutputView;
-
-import java.util.List;
 
 public class Application {
     // TODO 구현 진행
     public static void main(String[] args) {
-        final List<Table> tables = TableRepository.tables();
-        OutputView.printTables(tables);
-
         MainController.run();
-
-        final List<Menu> menus = MenuRepository.menus();
-        OutputView.printMenus(menus);
     }
 }
