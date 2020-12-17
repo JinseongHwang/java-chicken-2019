dd61401464c2107594f176a111d79d5fe51905d4
JinseongHwang <eddy5360@naver.com>
2020-12-18 02:18:15 +0900

    feat: 주문 수량 검증 구현

--- src/main/java/utils/Validator.java
+++ src/main/java/utils/Validator.java
@@ -1,9 +1,13 @@
 package utils;
 
+import domain.Menu;
+import domain.Table;
+
 public class Validator {
 
     private static final int MIN_MAIN_FUNCTION_NUMBER = 1;
     private static final int MAX_MAIN_FUNCTION_NUMBER = 3;
+    private static final int MAX_ORDERABLE_QUANTITY = 99;
 
     public static int onlyMainFunction(int number) {
         if (MIN_MAIN_FUNCTION_NUMBER <= number && number <= MAX_MAIN_FUNCTION_NUMBER) {
@@ -12,6 +16,11 @@ public class Validator {
         throw new IllegalArgumentException();
     }
 
-
+    public static int reasonableQuantity(Table table, Menu menu, int quantity) {
+        if (quantity > 0 && table.orderedQuantity(menu) + quantity <= MAX_ORDERABLE_QUANTITY) {
+            return quantity;
+        }
+        throw new IllegalArgumentException();
+    }
 
 }
