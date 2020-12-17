72088638305921a0cfb2ba9d203290b15c6b2a9c
JinseongHwang <eddy5360@naver.com>
2020-12-18 02:19:10 +0900

    feat: 테이블에 주문 내역을 저장하는 것을 구현

--- src/main/java/domain/Table.java
+++ src/main/java/domain/Table.java
@@ -1,7 +1,11 @@
 package domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Table {
     private final int number;
+    private Map<Menu, Integer> orders = new HashMap<>();
 
     public Table(final int number) {
         this.number = number;
@@ -11,4 +15,20 @@ public class Table {
     public String toString() {
         return Integer.toString(number);
     }
+
+    public int orderedQuantity(Menu menu) {
+        if (orders.get(menu) == null) {
+            return 0;
+        }
+        return orders.get(menu);
+    }
+
+    public void add(Menu menu, int quantity) {
+        orders.put(menu, orderedQuantity(menu) + quantity);
+    }
+
+    public Map<Menu, Integer> orderContents() {
+        return orders;
+    }
+
 }
