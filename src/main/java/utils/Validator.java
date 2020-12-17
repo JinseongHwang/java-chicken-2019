86f0fffc7735901368ff34f202ba30e41ac6de13
JinseongHwang <eddy5360@naver.com>
2020-12-18 03:18:52 +0900

    feat: 유효한 계산 방법인지 검증

--- src/main/java/utils/Validator.java
+++ src/main/java/utils/Validator.java
@@ -8,6 +8,8 @@ public class Validator {
     private static final int MIN_MAIN_FUNCTION_NUMBER = 1;
     private static final int MAX_MAIN_FUNCTION_NUMBER = 3;
     private static final int MAX_ORDERABLE_QUANTITY = 99;
+    private static final int METHOD_CREDIT_CARD = 1;
+    private static final int METHOD_CASH = 2;
 
     public static int onlyMainFunction(int number) {
         if (MIN_MAIN_FUNCTION_NUMBER <= number && number <= MAX_MAIN_FUNCTION_NUMBER) {
@@ -19,6 +21,13 @@ public class Validator {
     public static int reasonableQuantity(Table table, Menu menu, int quantity) {
         if (quantity > 0 && table.orderedQuantity(menu) + quantity <= MAX_ORDERABLE_QUANTITY) {
             return quantity;
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public static int reasonableMethod(int method) {
+        if (method == METHOD_CREDIT_CARD || method == METHOD_CASH) {
+            return method;
         }
         throw new IllegalArgumentException();
     }
