4122fdeaa1637244894344724484b36261d7ffed
JinseongHwang <eddy5360@naver.com>
2020-12-18 02:21:03 +0900

    feat: 메뉴 선택 및 수량 입력 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,7 @@
 package view;
 
+import domain.Menu;
+import domain.MenuRepository;
 import domain.Table;
 import domain.TableRepository;
 import utils.Validator;
@@ -43,6 +45,38 @@ public class InputView {
         } catch (IllegalArgumentException IAE) {
             System.out.println("[ERROR] 존재하지 않는 테이블 번호입니다.");
             return inputTableNumber();
+        }
+    }
+    
+    public static Menu inputMenuNumber() {
+        try {
+            System.out.println("## 등록할 메뉴를 선택하세요.");
+            int menuNumber = scanner.nextInt();
+            bufferFlush();
+            return MenuRepository.getMenuByNumber(menuNumber);
+        } catch (InputMismatchException IME) {
+            System.out.println("[ERROR] 정수를 입력해주세요.");
+            bufferFlush();
+            return inputMenuNumber();
+        } catch (IllegalArgumentException IAE) {
+            System.out.println("[ERROR] 존재하지 않는 메뉴 번호입니다.");
+            return inputMenuNumber();
+        }
+    }
+
+    public static int inputMenuQuantity(Table table, Menu menu) {
+        try {
+            System.out.println("## 메뉴의 수량을 입력하세요.");
+            int quantity = scanner.nextInt();
+            bufferFlush();
+            return Validator.reasonableQuantity(table, menu, quantity);
+        } catch (InputMismatchException IME) {
+            System.out.println("[ERROR] 정수를 입력해주세요.");
+            bufferFlush();
+            return inputMenuQuantity(table, menu);
+        } catch (IllegalArgumentException IAE) {
+            System.out.println("[ERROR] 1~99의 값만 입력 가능하며, 이 메뉴의 총 주문량은 99를 초과할 수 없습니다.");
+            return 0;
         }
     }
 
